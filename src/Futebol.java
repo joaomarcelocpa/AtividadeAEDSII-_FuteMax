7dc223afa1d7f9e96415bec9ecee06d319d58a51
joaomarcelocpa <joaomarcelocpa0303@gmail.com>
2025-04-23 15:38:52 -0300

    implementação da atividade futemax

--- src/Futebol.java
+++ src/Futebol.java
@@ -2,44 +2,69 @@ package src;
 
 import java.util.Scanner;
 
-/*
- * Problema:
- * 
- * "Dado um array de skills de jogadores de futebol, determinar o 
- * maior time balanceado possível em que a diferença de skills
- * entre quaisquer pares de jogadores no mesmo time não seja
- * maior que 5."
- * 
- * Exemplos de input/output: tests/
- * 
- * Não é permitido o uso da biblioteca Arrays.
- * É permitido o uso de códigos próprios
- * desenvolvidas em aulas anteriores.
- * 
- * Ver especificação em pdf para mais detalhes.
-*/
 public class Futebol {
+
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
 
-        // Leitura do número de jogadores
+        // leitura do número de jogadores
         int n = leitor.nextInt();
         int[] skills = new int[n];
-        
-        // Leitura das skills
+
+        // leitura das skills
         for (int i = 0; i < n; i++) {
             skills[i] = leitor.nextInt();
         }
         leitor.close();
-        
-        // Cálculo do maior time possível
+
+        // cálculo do maior time possível
         int maxTeamSize = maiorTimePossivel(skills);
 
-        System.out.println(maxTeamSize);
+        System.out.println("O tamanho maximo do time: " + maxTeamSize);
+        System.out.println("A complexidade de tempo total do algoritmo = O(n^2), sendo O(n) para a para a verificao do tamanho do time e O(n^2) para a ordenacao");
     }
 
+
     private static int maiorTimePossivel(int[] skills) {
-        // Implementar a lógica para calcular o maior time possível
-        throw new UnsupportedOperationException("Não implementado");
+        // ordena o vetor de skills
+        ordenar(skills);
+
+        int maxTamanho = 0;
+        int inicio = 0;
+
+        // percorre o vetor de skills
+        for (int fim = 0; fim < skills.length; fim++){
+
+            // enquanto a diferença entre o maior e o menor skill for maior que 5, incrementa o início
+            while (skills[fim] - skills[inicio] > 5){
+                inicio++;
+            }
+
+            // calcula o tamanho atual do time
+            int tamanhoAtual = fim - inicio + 1;
+
+            // atualiza o tamanho máximo do time
+            if (tamanhoAtual > maxTamanho){
+                maxTamanho = tamanhoAtual;
+            }
+        }
+        return maxTamanho;
+    }
+
+
+    private static void ordenar(int[] vetor) {
+
+        int n = vetor.length;
+
+        // Ordenação Bubble Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (vetor[j] > vetor[j + 1]) {
+                    int temp = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = temp;
+                }
+            }
+        }
     }
 }
